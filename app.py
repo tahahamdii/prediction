8baf9a4fb6b8739377718f75ca0149bb4d0d91d8
Hamdi Taha <hamdi.taha@esprit.tn>
2024-08-06 15:41:07 +0200

    prompt template

--- app.py
+++ app.py
@@ -13,3 +13,18 @@ from langchain.chains import RetrievalQA
 # bring in our GROQ_API_KEY
 from dotenv import load_dotenv
 load_dotenv()
+
+groq_api_key = os.getenv("GROQ_API_KEY")
+qdrant_url = os.getenv("QDRANT_URL")
+qdrant_api_key = os.getenv("QDRANT_API_KEY")
+
+custom_prompt_template = """Use the following pieces of information to answer the user's question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+Context: {context}
+Question: {question}
+
+Only return the helpful answer below and nothing else.
+Helpful answer:
+"""
+
